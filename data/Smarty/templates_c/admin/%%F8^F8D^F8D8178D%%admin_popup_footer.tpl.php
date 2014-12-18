be324e9c90175951c27d28f6f560ca4fa7b38d79
unknown <lntan@lntan.tma.com.vn>
2014-12-18 19:30:22 +0700

    First commit

---
+++ data/Smarty/templates_c/admin/%%F8^F8D^F8D8178D%%admin_popup_footer.tpl.php
@@ -0,0 +1,6 @@
+<?php /* Smarty version 2.6.27, created on 2014-12-16 16:39:19
+         compiled from G:/Lockon/eccube/html/../data/Smarty/templates/admin/admin_popup_footer.tpl */ ?>
+
+</div>
+</body>
+</html>
